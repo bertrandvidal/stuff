8743b9f1b9df99c9032c9142da6dbf5617592359
bvidal <bvidal@ltutech.com>
2014-02-04 23:11:15 -0500

    Using the 'pacman' technique
    The pacman technique is computing the result as we go - sort of ..,
    The idea is to always maintain the highest values for each position in the line.
    We only need to get the max at the end.
    
    The one trick is that we are lucky that list[-1] is the last element that's why
    we append zero to the var that holds the current values at each each iteration
    we can add this 0 to the first element of the current line of the triangle
    
    Tests:
    732506 is the expected output as this was the only email that I was able to send
    to the company that created this exercice.

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,22 +1,35 @@
 import os
+import sys
 
-file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "small_triangle.txt"))
+triangle_file = sys.argv[1] if len(sys.argv) == 2 else "big_triangle.txt"
 
-with open(file_path, "r") as triangle:
-  lines = triangle.readlines()
+print "Using '%s'" % triangle_file
 
-base = [int(v.strip()) for v in lines[-1].split(" ") if v.strip()]
-lines = lines[:-1]
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                         triangle_file))
 
-print base
+# method to integerify each entry in the file
+def clean_line(line):
+  return [int(e.strip()) for e in line.split(" ")]
 
-for line in lines:
-  values = [int(v.strip()) for v in line.split(" ") if v.strip()]
-  if len(values) == 1:
-    base[0] += values[0]
-    continue
-  for idx, value in enumerate(values):
-    print "base[%s]:"%(idx-1), base[idx+1] + values[idx], base[idx+1] + values[idx-1]
-    base[idx+1] += max(values[idx], values[idx-1])
-  print base
+# triangle is a list of list containing the numbers
+with open(file_path, "r") as triangle_file:
+  triangle = map(clean_line, triangle_file.readlines())
+
+# base maintains the current 'total'
+base = triangle.pop(0)
+
+# Each line of the triangle is modified inline which forces us to copy
+# the line back into base
+for line in triangle:
+  # We add this extra entry so we can use the trick where list[-1] is
+  # the last element of the list
+  base.append(0)
+  for index, entry in enumerate(line):
+    # The current entry is added to the max between the number directly
+    # above and the number above and one to the left - potentially the
+    # last number of the list when index - 1 = -1
+    line[index] = entry + max(base[index], base[index -1])
+  base = line[:]
+
 print max(base)
