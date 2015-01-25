55c59efcd8969b79a79246c1409e3f70e89e275a
bertrandvidal <vidal.bertrand@gmail.com>
2015-01-25 15:10:08 -0500

    Make Django Admin more user friendly
    We make the Admin panel more user friendly by having 3 extra Choices ready
    to be created when adding a Question, search and filter can be applied to the
    Question change list and we display other fields/method i.e. pub_date and
    was_published_recently

--- djangoprojects/mysite/polls/admin.py
+++ djangoprojects/mysite/polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date info', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently',
+                    'time_since_pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
