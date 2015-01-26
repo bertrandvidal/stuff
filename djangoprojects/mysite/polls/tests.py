b405a6ee24f18c54f93eb11da62c43e62f7a21bd
bertrandvidal <vidal.bertrand@gmail.com>
2015-01-26 18:27:51 +0100

    Add tests for detail view

--- djangoprojects/mysite/polls/tests.py
+++ djangoprojects/mysite/polls/tests.py
@@ -17,6 +17,22 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
+class QuestionIndexDetailTest(TestCase):
+
+    def test_detail_view_with_future_question(self):
+        future_question = create_question("Future question", 30)
+        response = self.client.get(reverse("polls:detail",
+                                           args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_past_question(self):
+        past_question = create_question("Past question", -12)
+        response = self.client.get(reverse("polls:detail",
+                                           args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
+
+
 class QuestionViewTests(TestCase):
 
     def test_index_view_with_no_questions(self):
