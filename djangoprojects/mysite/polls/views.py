75b67a8452266eb11c01cd0ee67640cdf6fe84b3
bertrandvidal <vidal.bertrand@gmail.com>
2015-01-26 10:34:32 +0100

    Add new urls for polls
    Polls can be listed and we get details on Question and Choice

--- djangoprojects/mysite/polls/views.py
+++ djangoprojects/mysite/polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from polls.models import Question
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." %
+                        question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_questions])
+    return HttpResponse(output)
