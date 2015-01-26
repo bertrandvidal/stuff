39da6203c1e797e498775b0d69b0a1e2c0798fd5
bertrandvidal <vidal.bertrand@gmail.com>
2015-01-26 11:26:08 +0100

    Properly display the results of a poll

--- djangoprojects/mysite/polls/views.py
+++ djangoprojects/mysite/polls/views.py
@@ -10,8 +10,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." %
-                        question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
