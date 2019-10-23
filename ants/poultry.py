9c97ae1d3b74b7f2bc0417861f98023ddc583be2
bertrandvidal <bvidal@crowdtwist.com>
2019-10-22 21:15:17 -0400

    [ANTS] Add much needed docblock

--- ants/poultry.py
+++ ants/poultry.py
@@ -4,25 +4,48 @@ from hashlib import md5
 
 
 class PoultryAndAnts:
+    """
+    See http://followthewhiterabbit.trustpilot.com/cs/step3.html
+    """
 
-    def __init__(self, anagram, target):
+    def __init__(self, anagram, targets):
+        """
+        :param anagram: anagram that we are looking for
+        :param targets: list of md5s of anagrams that would match the given
+        anagram
+        """
         self.anagram_counter = Counter(anagram)
         self.anagram_length = len(anagram)
-        self.target = target
+        self.targets = targets
 
     def find_match(self, node):
+        """ Given a fully built tree return the first anagram found that has
+        an md5 found in self.targets. Recursive method visiting the given
+        node first then its children in order they were added.
+
+        :param node: the node being visited
+        :return: the first found list of words that have an md5 found in
+        self.targets, None if no such list of words can be found
+        """
         (words, length, _, children) = node
         # we've reached a leaf node that has the right amount of letters
         if not children and length == self.anagram_length:
             # check for md5 of all permutations with white spaces
             for perm in permutations(words):
                 digest = md5(" ".join(perm)).hexdigest()
-                if digest in self.target:
-                    print "\n\nWINNER!! - %s \n\n" % list(perm)
+                if digest in self.targets:
+                    return list(perm)
         for child in children:
             self.find_match(child)
 
     def nb_leaf_nodes(self, node, nb_leaves):
+        """ Informational method to get the number of leaf nodes that can be
+        reached from the given node. Recursive method.
+
+        :param node: node being visited
+        :param nb_leaves: current number of leaf node found so far
+        :return: number of leaf nodes that can be reached from the given node
+        """
         (_, __, ___, children) = node
         if not children:
             return 1
@@ -32,6 +55,14 @@ class PoultryAndAnts:
         return nb_leaves + r
 
     def add_node(self, node, new_word, new_word_counter):
+        """ Add 'new_word' to the given node and to any of its children if
+        'word_contains' returns true.
+
+        :param node: current node that should get 'new_word' added to
+        :param new_word: the word to add to the current node
+        :param new_word_counter: the Counter from 'new_word'
+        :return: None
+        """
         (words, length, counter, children) = node
         for c in children:
             self.add_node(c, new_word, new_word_counter)
@@ -43,9 +74,27 @@ class PoultryAndAnts:
                 (words + [new_word], new_node_length, new_node_counter, []))
 
     def word_contains(self, needle, haystack):
-        return all(haystack[k] - v >= 0 for k,v in needle.items())
+        """ Check whether or not the needle (Counter of a node) can be
+        fit in haystack (Counter of anagram). A Counter is said to fit within
+        another Counter if for common letters the haystack has more occurrences.
+
+        :param needle: Counter of a single word
+        :param haystack: Counter of the anagram
+        :return: true if the needle fit in the haystack
+        """
+        return all(haystack[k] - v >= 0 for k, v in needle.items())
 
     def find_anagram(self, wordlist):
+        """ Build a tree of nodes that only contain letter from the anagram
+        and each letter will appear no more times that it does in the anagram
+
+        Node: ([words], sum length of [words], Counter([words]), [children])
+
+        TODO: output should not be mingled with the logic
+
+        :param wordlist: the word list to use to find an anagram
+        :return: None
+        """
         i = 0
         for word in set(wordlist):
             try:
