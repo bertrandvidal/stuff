743fc0c39de08df64d3d314a06fd784a722c01fa
bertrandvidal <bert.vidal@oracle.com>
2020-11-26 18:14:55 -0500

    [LRU] Add cache's capacity
    This will be the maximum amount of items that can be held in the cache.
    
    Tests:
    - lru tests OK

--- caching/lru.py
+++ caching/lru.py
@@ -3,4 +3,10 @@ class LeastRecentlyUsedCache:
     """
     Offers fix memory caching where least recently used entries are evicted.
     """
-    pass
+    _DEFAULT_CAPACITY = 10
+
+    def __init__(self, capacity: int = _DEFAULT_CAPACITY):
+        self._capacity = capacity
+
+    def get_capacity(self):
+        return self._capacity
--- caching/test_lru.py
+++ caching/test_lru.py
@@ -1,5 +1,15 @@
 from unittest import TestCase
 
+from lru import LeastRecentlyUsedCache
+
 
 class TestLeastRecentlyUsedCache(TestCase):
-    pass
+
+    def test_default_capacity(self):
+        lru = LeastRecentlyUsedCache()
+        self.assertIsNotNone(lru.get_capacity())
+
+    def test_constructor_capacity(self):
+        capacity = 12
+        lru = LeastRecentlyUsedCache(capacity)
+        self.assertEqual(lru.get_capacity(), capacity)
