4d6193d1ac4d6992200f6f879f012ac5ad634e82
bertrandvidal <bert.vidal@oracle.com>
2021-01-16 12:58:13 -0500

    [AUDIO-IMG] Create visualization of the wave data
    Well that didn't work either ... it doesn't give much visually ... will
    try the numpy array approach

--- audio_to_img_to_audio/img_to_audio.py
+++ audio_to_img_to_audio/img_to_audio.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import struct
 import sys
-import wave
 
 from PIL import Image
 
@@ -52,33 +50,50 @@ image_range = height
 # range of a 16-bit wave
 wave_min = -32768
 wave_max = 32767
-
-
-# TODO(bvidal): only for visual debug
-sampleRate = 4800
-duration = 1
-wave_min = 0
-wave_max = int(4800 / width)
-
 wave_range = wave_max - wave_min
 
 
-def scale_value(v):
+def image_to_wave_scale(v):
     return int(v * wave_range / image_range) + wave_min
 
 
+def scale(v, from_min, from_max, to_min, to_max):
+    return int(to_min + (
+            ((v - from_min) / (from_max - from_min))
+            * (to_max - to_min)
+    ))
+
+
 prev_avg = 0
+wave_values = []
 for idx, (min_val, max_val) in enumerate(min_max):
     original_average = max_val + int((min_val - max_val) / 2)
     debug_image.putpixel((idx, original_average), (0, 0, 255))
-    scaled_avg = scale_value(original_average)
+    scaled_avg = scale(original_average, 0, height, wave_min, wave_max)
     # we gradually go from the previous avg to the current one
     per_frame_diff = int((scaled_avg - prev_avg) / wave_frame)
     for frame in range(wave_frame):
         value = prev_avg + (frame * per_frame_diff)
         if value > wave_max or value < wave_min:
             print(f"bad value: {value} @ index {idx} @ frame {frame}")
+        wave_values.append(value)
     prev_avg = scaled_avg
 
 with open("debug-%s" % sys.argv[1], "wb") as dbg_file:
     debug_image.save(dbg_file)
+
+wave_viz_w, wave_viz_h = (int(len(wave_values) / 8), int(wave_range / 100))
+# Entirely black image
+wave_visualization = Image.new("RGB", (wave_viz_w, wave_viz_h), color=0)
+
+with open("wave-debug-%s" % sys.argv[1], "wb") as wave_dbg_file:
+    # we "sample" the wave values to adapt to the wave_viz width
+    for idx, wave_value in enumerate(wave_values[:wave_viz_w]):
+        try:
+            wave_viz_value = scale(wave_value, wave_min, wave_max, 0, wave_viz_h)
+            wave_visualization.putpixel((idx, wave_viz_value),
+                                        (0, 255, 255))
+        except Exception as e:
+            print(f"@{idx}: scale({wave_value}) = {wave_viz_value}")
+            raise e
+    wave_visualization.save(wave_dbg_file)
