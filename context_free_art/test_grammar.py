1990fd757e1ebff4a289955aa233cf91629db27c
bertrandvidal <bert.vidal@oracle.com>
2021-02-27 16:24:09 -0500

    [CFART] Add more test for generator

--- context_free_art/test_grammar.py
+++ context_free_art/test_grammar.py
@@ -7,6 +7,7 @@ from pixel import Pixel
 class GrammarGeneratorTest(unittest.TestCase):
     _identity_grammar = Grammar(rules=[lambda p: [p]])
     _duplicate_grammar = Grammar(rules=[lambda p: [p, p]])
+    _next_diagonal_grammar = Grammar(rules=[lambda p: [p, Pixel(x=p.x + 1, y=p.y + 1)]])
 
     def test_nb_iteration(self):
         generator = GrammarGenerator(grammar=self._identity_grammar, start=Pixel())
@@ -16,6 +17,12 @@ class GrammarGeneratorTest(unittest.TestCase):
         generator = GrammarGenerator(grammar=self._duplicate_grammar, start=Pixel())
         for iteration in generator.generate(5):
             self.assertEqual(len(iteration), 1)
+
+    def test_existing_pixels_are_retained(self):
+        n = 10
+        generator = GrammarGenerator(grammar=self._next_diagonal_grammar, start=Pixel())
+        # n + 1 because the n iteration add element to start so: n + start
+        self.assertEqual(len(list(generator.generate(n))[-1]), n + 1)
 
 
 if __name__ == '__main__':
