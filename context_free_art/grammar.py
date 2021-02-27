ebf27d9f04bfbc8c5a66cdf4313f1e290b93c165
bertrandvidal <bert.vidal@oracle.com>
2021-02-27 16:06:35 -0500

    [CFART] Introduce simple GrammarGenerator

--- context_free_art/grammar.py
+++ context_free_art/grammar.py
@@ -13,3 +13,30 @@ class Grammar:
     Contains the set of Rules that define the entirety of the Grammar.
     """
     rules: List[Rule]
+
+
+@dataclass
+class GrammarGenerator:
+    """
+    Given a Grammar and a starting point (a Pixel) generate an iterator for all sequences of the Grammar.
+    """
+    grammar: Grammar
+    start: Pixel
+
+    def generate(self):
+        """
+        Generate 10 iterations for the Grammar
+
+        :return: iterator for sequence of the grammar
+        """
+        def _iterator():
+            generated_pixels = [self.start]
+            for _ in range(10):
+                pixels = generated_pixels
+                generated_pixels = []
+                for pixel in pixels:
+                    for rule in self.grammar.rules:
+                        generated_pixels.extend(rule(pixel))
+                yield generated_pixels
+
+        return _iterator()
