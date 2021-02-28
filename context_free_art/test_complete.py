71b33a596b1066f2eca984eae29853cbb605e8ca
bertrandvidal <bert.vidal@oracle.com>
2021-02-28 12:02:26 -0500

    [CFART] Add a 'complete' test to facilitate ... well testing

---
+++ context_free_art/test_complete.py
@@ -0,0 +1,33 @@
+import unittest
+from random import randint
+from typing import List
+
+from display import Terminal
+from grammar import Grammar, GrammarGenerator, Rule
+from pixel import Pixel
+
+
+class RandomRule(Rule):
+
+    def __call__(self, pixel: Pixel, width: int, height: int) -> List[Pixel]:
+        return [pixel, Pixel(x=pixel.x + randint(-1, 1), y=pixel.y + randint(-1, 1))]
+
+
+class CompleteTest(unittest.TestCase):
+
+    def test_something(self):
+        size = 25
+        start = Pixel()
+        display = Terminal(size)
+        rules = [RandomRule()]
+        grammar = Grammar(rules)
+        generator = GrammarGenerator(grammar, start, display)
+        for generated_pixels in generator.generate(50):
+            print("-" * 25)
+            display.display(generated_pixels)
+            print("-" * 25)
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
