89ec2471574059143597fc4e28bd30e86e575f49
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 18:48:32 -0400

    [PIXEL] Fill out main method
    The main method follows a simple algorithm:
    - build chamber from input
    - while there are pixels to move
        - move pixel by the given speed
        - get the textual representation of the chamber's state
    - print output
    
    Tests:
    - ./gradlew build OK
    - java -cp ./build/libs/pixels-1.0-SNAPSHOT.jar
      com.bert.pixels.Animation "r.......y" 1 > correct output

--- pixels/src/main/java/com/bert/pixels/Animation.java
+++ pixels/src/main/java/com/bert/pixels/Animation.java
@@ -1,13 +1,28 @@
 package com.bert.pixels;
 
-import java.util.Arrays;
+import com.bert.pixels.models.Chamber;
+import com.bert.pixels.view.TextVisualization;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Entry point to solve the pixel exercise
  */
 public class Animation {
 
+  private final static int MAX_SIZE = 10;
+
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    final int speed = Integer.parseInt(args[1]);
+    TextVisualization visualization = new TextVisualization(MAX_SIZE);
+    final Chamber chamber = visualization.from(args[0]);
+    // We could try to get the max nb of iteration by using the size of the chamber and the speed
+    // and only allocate that many entries in the list #prematureoptimization
+    List<String> iterations = new ArrayList<>();
+    while (!chamber.isEmpty()) {
+      iterations.add(visualization.to(chamber.movePixels(speed)));
+    }
+    System.out.println(String.join("\n", iterations));
   }
 }
