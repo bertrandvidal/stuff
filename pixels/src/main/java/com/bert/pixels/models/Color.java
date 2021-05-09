cf0d448bdc7274efc3ec3c6785f91c31a6d3b418
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 19:01:57 -0400

    [PIXELS] Add Color.getMovement
    This method will be replacing the 'apply' method.
    The name 'getMovement' "flows" better when reading.
    
    Tests:
    - ColorTest OK

--- pixels/src/main/java/com/bert/pixels/models/Color.java
+++ pixels/src/main/java/com/bert/pixels/models/Color.java
@@ -31,6 +31,13 @@ public enum Color {
   }
 
   /**
+   * @return the "movement" to apply to a pixel's position to get its new position
+   */
+  public BinaryOperator<Integer> getMovement() {
+    return this.moveFunction;
+  }
+
+  /**
    * @return the single character representation of the Color
    */
   public Character toChar() {
--- pixels/src/test/java/com/bert/pixels/models/ColorTest.java
+++ pixels/src/test/java/com/bert/pixels/models/ColorTest.java
@@ -18,6 +18,16 @@ class ColorTest {
   }
 
   @Test
+  void testGetMovementRedColor() {
+    assertEquals(Color.R.getMovement().apply(0, 1), 1);
+  }
+
+  @Test
+  void testGetMovementYellowColor() {
+    assertEquals(Color.Y.getMovement().apply(0, 1), -1);
+  }
+
+  @Test
   void testToChar() {
     assertEquals(Color.R.toChar(), 'R');
     assertEquals(Color.Y.toChar(), 'Y');
