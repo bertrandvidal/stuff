6f55ad13a685959dc5ef3961563b1ef391168ca0
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 16:02:01 -0400

    Add necessary Gradle machinery for the project
    Contains all the necessary files and machinery for a Gradle project.
    
    Tests:
    - ran main class :+1:

---
+++ pixels/src/main/java/com/bert/pixels/Animation.java
@@ -0,0 +1,13 @@
+package com.bert.pixels;
+
+import java.util.Arrays;
+
+/**
+ * Entry point to solve the pixel exercise
+ */
+public class Animation {
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
+  }
+}
