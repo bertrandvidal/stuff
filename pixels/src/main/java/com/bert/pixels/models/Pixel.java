be6bf667515a46cec727bf8c50994e4dde319fc4
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 16:13:33 -0400

    [PIXELS] Add a simple Pixel class
    For now Pixel is a simple data object, also add some simplistic tests.
    
    Tests:
    - PixelTest OK

---
+++ pixels/src/main/java/com/bert/pixels/models/Pixel.java
@@ -0,0 +1,29 @@
+package com.bert.pixels.models;
+
+/**
+ * A pixel is capable of moving within a "chamber", it is defined by its color and current position.
+ */
+public final class Pixel {
+
+  private final Color color;
+  private Integer position;
+
+  public Pixel(Color color, Integer position) {
+    this.color = color;
+    this.position = position;
+  }
+
+  /**
+   * @return the color of the pixel
+   */
+  public Color getColor() {
+    return this.color;
+  }
+
+  /**
+   * @return the current position of the pixel
+   */
+  public Integer getPosition() {
+    return this.position;
+  }
+}
---
+++ pixels/src/test/java/com/bert/pixels/models/PixelTest.java
@@ -0,0 +1,24 @@
+package com.bert.pixels.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class PixelTest {
+
+  @Test
+  void testGetColor() {
+    for (Color color: Color.values()) {
+      assertEquals(new Pixel(color, 1).getColor(), color);
+    }
+  }
+
+  @Test
+  void testGetPosition() {
+    assertEquals(new Pixel(Color.RED, -1).getPosition(), -1);
+    assertEquals(new Pixel(Color.RED, 0).getPosition(), 0);
+    assertEquals(new Pixel(Color.RED, 1).getPosition(), 1);
+    assertNull(new Pixel(Color.RED, null).getPosition());
+  }
+}
