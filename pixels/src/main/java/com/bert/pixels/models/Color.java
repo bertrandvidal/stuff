ce1d435c1ac998ad89000fe195c0624ed902cc90
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 16:03:42 -0400

    [PIXELS] Add enum for pixel colors
    Tests:
    - none

---
+++ pixels/src/main/java/com/bert/pixels/models/Color.java
@@ -0,0 +1,15 @@
+package com.bert.pixels.models;
+
+/**
+ * Possible colors for a pixel
+ */
+public enum Color {
+  /**
+   * Red pixels can only more right.
+   */
+  RED,
+  /**
+   * Yellow pixels can only more left.
+   */
+  YELLOW
+}
