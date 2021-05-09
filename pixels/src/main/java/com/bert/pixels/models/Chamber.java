c995ecebde0ff34d2b88e4844714e3199c6d2bd4
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 16:36:22 -0400

    [PIXELS] Add method to check is a Chamber is empty
    This will be useful to iterate until all pixels are out of the Chamber
    
    Tests:
    - ChamberTest OK

--- pixels/src/main/java/com/bert/pixels/models/Chamber.java
+++ pixels/src/main/java/com/bert/pixels/models/Chamber.java
@@ -5,8 +5,13 @@ import java.util.List;
 /**
  * Hold the currently visible pixels
  */
-public class Chamber {
+public final class Chamber {
+
   private List<Pixel> pixels;
+
+  public Chamber(List<Pixel> pixels) {
+    this.pixels = pixels;
+  }
 
   /**
    * Note that we should probably return an immutable list of pixels.
@@ -15,5 +20,12 @@ public class Chamber {
    */
   public List<Pixel> pixels(){
     return this.pixels;
+  }
+
+  /**
+   * @return true is no pixel is visible in the chamber, false otherwise
+   */
+  public boolean isEmpty() {
+    return this.pixels.isEmpty();
   }
 }
---
+++ pixels/src/test/java/com/bert/pixels/models/ChamberTest.java
@@ -0,0 +1,23 @@
+package com.bert.pixels.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ChamberTest {
+
+  @Test
+  void testIsEmptyNoPixel() {
+    assertTrue(new Chamber(new ArrayList<>()).isEmpty());
+  }
+
+  @Test
+  void testIsEmptyWithPixel() {
+    final ArrayList<Pixel> pixels = new ArrayList<>();
+    pixels.add(new Pixel(Color.RED, 1));
+    assertFalse(new Chamber(pixels).isEmpty());
+  }
+}
