bf25d026d76086d166d0cf85097b381bc194d569
bertrandvidal <bert.vidal@oracle.com>
2021-05-09 17:46:16 -0400

    [PIXELS] Add method Chamber::movePixels
    The Chamber will move the pixels according to their color, if they are
    out of bound they are removed.
    
    Tests:
    - ChamberTest OK

--- pixels/src/main/java/com/bert/pixels/models/Chamber.java
+++ pixels/src/main/java/com/bert/pixels/models/Chamber.java
@@ -1,5 +1,6 @@
 package com.bert.pixels.models;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -8,9 +9,11 @@ import java.util.List;
 public final class Chamber {
 
   private List<Pixel> pixels;
+  private final Integer size;
 
-  public Chamber(List<Pixel> pixels) {
+  public Chamber(List<Pixel> pixels, Integer size) {
     this.pixels = pixels;
+    this.size = size;
   }
 
   /**
@@ -27,5 +30,23 @@ public final class Chamber {
    */
   public boolean isEmpty() {
     return this.pixels.isEmpty();
+  }
+
+  /**
+   * Move pixels in the chamber. Pixels that exit the chamber are removed.
+   * @param speed the speed at which the pixels move in the chamber
+   * @return the current chamber updated.
+   */
+  public Chamber movePixels(int speed) {
+    final Iterator<Pixel> iterator = this.pixels.iterator();
+    while (iterator.hasNext()) {
+      final Pixel pixel = iterator.next();
+      pixel.move(speed);
+      final Integer position = pixel.getPosition();
+      if (position < 0 || position >= this.size) {
+        iterator.remove();
+      }
+    }
+    return this;
   }
 }
--- pixels/src/test/java/com/bert/pixels/models/ChamberTest.java
+++ pixels/src/test/java/com/bert/pixels/models/ChamberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,13 +12,41 @@ class ChamberTest {
 
   @Test
   void testIsEmptyNoPixel() {
-    assertTrue(new Chamber(new ArrayList<>()).isEmpty());
+    assertTrue(new Chamber(new ArrayList<>(), -1).isEmpty());
   }
 
   @Test
   void testIsEmptyWithPixel() {
     final ArrayList<Pixel> pixels = new ArrayList<>();
     pixels.add(new Pixel(Color.R, 1));
-    assertFalse(new Chamber(pixels).isEmpty());
+    assertFalse(new Chamber(pixels, -1).isEmpty());
+  }
+
+  @Test
+  void testMovePixels() {
+    final ArrayList<Pixel> pixels = new ArrayList<>();
+    pixels.add(new Pixel(Color.R, 0));
+    pixels.add(new Pixel(Color.Y, 1));
+    final Chamber chamber = new Chamber(pixels, 2);
+    chamber.movePixels(1);
+    assertFalse(chamber.isEmpty());
+    assertEquals(chamber.pixels().size(), 2);
+    for (Pixel pixel : chamber.pixels()) {
+      if (Color.R.equals(pixel.getColor())) {
+        assertEquals(pixel.getPosition(), 1);
+      } else {
+        assertEquals(pixel.getPosition(), 0);
+      }
+    }
+  }
+
+  @Test
+  void testMovePixelsOutOfBoundAreRemoved() {
+    final ArrayList<Pixel> pixels = new ArrayList<>();
+    pixels.add(new Pixel(Color.R, 1));
+    pixels.add(new Pixel(Color.Y, 0));
+    final Chamber chamber = new Chamber(pixels, 2);
+    chamber.movePixels(1);
+    assertTrue(chamber.isEmpty());
   }
 }
