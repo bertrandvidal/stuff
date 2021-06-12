b3bad13f86b54f6d218062948a4090200e92c914
bertrandvidal <bert.vidal@oracle.com>
2021-06-12 09:53:53 -0400

    [PIXELS] Add more assertions to Pixel's equals test

--- pixels/src/test/java/com/bert/pixels/models/PixelTest.java
+++ pixels/src/test/java/com/bert/pixels/models/PixelTest.java
@@ -2,7 +2,10 @@ package com.bert.pixels.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class PixelTest {
@@ -38,5 +41,11 @@ class PixelTest {
     Pixel redPixel = new Pixel(Color.RED, 0);
     assertEquals(redPixel, redPixel);
     assertEquals(redPixel, new Pixel(Color.RED, 0));
+    assertNotEquals(redPixel, new Pixel(Color.RED, 1));
+    assertNotEquals(redPixel, new Pixel(Color.YELLOW, 0));
+    assertEquals(Collections.singleton(redPixel), Collections.singleton(redPixel));
+    assertEquals(Collections.singleton(redPixel), Collections.singleton(new Pixel(Color.RED, 0)));
+    assertNotEquals(Collections.singleton(redPixel), Collections.singleton(new Pixel(Color.RED, 1)));
+    assertNotEquals(Collections.singleton(redPixel), Collections.singleton(new Pixel(Color.YELLOW, 0)));
   }
 }
