3ce1bbdd71b68ebe32cc499b7811005b8fa38b9a
bertrandvidal <bert.vidal@oracle.com>
2021-06-12 10:47:44 -0400

    [PIXELS] Create the ORANGE color
    That color does not "naturally" occur and can only result out of the
    combination of RED and YELLOW. It's input representation will result in
    two Pixels being created at the given position (1 red, 1 yellow).
    
    When a red and yellow pixels move to the same position it results in an
    ORANGE pixel that will then "separate" back into the two original
    pixels.
    
    ORANGE could be considered a "virtual" pixel, for a lack of a better
    word.
    
    Tests:
    - ./gradlew test

--- pixels/src/test/java/com/bert/pixels/models/ColorTest.java
+++ pixels/src/test/java/com/bert/pixels/models/ColorTest.java
@@ -3,8 +3,11 @@ package com.bert.pixels.models;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ColorTest {
 
@@ -28,5 +31,9 @@ class ColorTest {
   void testPixelAt() {
     assertEquals(Color.RED.pixelAt(0), Collections.singleton(new Pixel(Color.RED, 0)));
     assertEquals(Color.YELLOW.pixelAt(0), Collections.singleton(new Pixel(Color.YELLOW, 0)));
+    Set<Pixel> pixels = new HashSet<>();
+    pixels.add(new Pixel(Color.RED, 0));
+    pixels.add(new Pixel(Color.YELLOW, 0));
+    assertTrue(Color.ORANGE.pixelAt(0).containsAll(pixels));
   }
 }
