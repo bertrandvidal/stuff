f5dce721a2a5a6ccb9d3a339e2e04a12f4fda8ab
bertrandvidal <vidal.bertrand@gmail.com>
2023-07-02 18:47:55 -0400

    Retrieve attribute from thermostat data / Read data from file

---
+++ thermostat/replay.py
@@ -0,0 +1,22 @@
+import json
+
+
+def get_thermostat_attribute(thermostat_data, attribute):
+    """Retrieve 'attribute' from 'thermostat_data' if it is present, None otherwise.
+
+    :param thermostat_data: dict containing thermostat data
+    :param attribute: name of the attribute to return, if present
+    :return: the value associate with 'attribute' if present, None otherwise.
+    """
+    return thermostat_data.get(attribute)
+
+
+def get_thermostat_data(file_path):
+    """Yield json objects as read from 'file_path', file is expected to be jsonl format.
+
+    :param file_path: path to a jsonl file - http://jsonlines.org/
+    :return: yield json objects from the given file path
+    """
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            yield json.loads(line.strip("\n"))
--- thermostat/replay_test.py
+++ thermostat/replay_test.py
@@ -1,10 +1,28 @@
 import unittest
 
+from replay import get_thermostat_attribute, get_thermostat_data
+
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_get_thermostat_attribute_has_attribute(self):
+        thermostat_data = {"attr-1": 12}
+        self.assertEqual(get_thermostat_attribute(thermostat_data, "attr-1"), 12)
+
+    def test_get_thermostat_attribute_no_attribute(self):
+        thermostat_data = {"attr-1": 12}
+        self.assertEqual(get_thermostat_attribute(thermostat_data, "not-attr-1"), None)
+
+    def test_get_thermostat_data_line_count(self):
+        self.assertEqual(len(list(get_thermostat_data("thermostat-data.jsonl"))), 1068)
+
+    def test_get_thermostat_data_jsonl_data(self):
+        iterator = get_thermostat_data("thermostat-data.jsonl")
+        first_element = next(iterator)
+        self.assertDictEqual(first_element, {"updateTime": "2016-01-01T00:43:00.001064", "update": {"ambientTemp": 80}})
+        last_element = list(iterator)[-1]
+        self.assertDictEqual(last_element, {"updateTime": "2016-12-31T05:09:00.020215",
+                                            "update": {"lastAlertTs": "2016-12-31T05:09:00.020215"}})
 
 
 if __name__ == '__main__':
